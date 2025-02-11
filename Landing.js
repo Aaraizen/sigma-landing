985b8f3ea58cb4485f27c5f2f099cf435481eec5
Aaraizen <aaraizenn@gmail.com>
2025-02-11 13:53:02 +0800

    fixed the skibidis in the navbar

--- Landing.js
+++ Landing.js
@@ -1,4 +1,3 @@
-console.log(document.getElementById("words").style.display);
 
 document.getElementById("post").onclick =function(){
     let Post = document.getElementsByClassName("Post");
@@ -63,42 +62,53 @@ document.getElementById("contact").onclick =function(){
     document.getElementById("about").style.color = "rgb(203, 219, 203)";
     document.getElementById("contact").style.color = "rgb(137, 248, 137)";
 }
+
+
+
+
 document.getElementById("span-holder").onclick = function(){
     document.getElementById("words").classList.toggle("show");
     
 }
+document.getElementById("span-holder1").onclick = function(){
+    document.getElementById("sub-words").classList.toggle("show");
+    
+}
+
+
+
 
 document.getElementById("sub-post").onclick = function(){
     document.getElementById("main-div").style.display = "none";
-    document.getElementById("sub-navbar").style.display = "flex";
+    document.getElementById("sub-navbar1").style.display = "flex";
     document.getElementById("posts-section").style.display = "flex";
     document.getElementById("blog-section").style.display = "none";
 }
 
 document.getElementById("view").onclick = function() {
     document.getElementById("main-div").style.display = "none";
-    document.getElementById("sub-navbar").style.display = "flex";
+    document.getElementById("sub-navbar1").style.display = "flex";
     document.getElementById("posts-section").style.display = "flex";
     document.getElementById("blog-section").style.display = "none";
 }
 
 document.getElementById("sub-blog").onclick = function() {
     document.getElementById("main-div").style.display = "none";
-    document.getElementById("sub-navbar").style.display = "flex";
+    document.getElementById("sub-navbar1").style.display = "flex";
     document.getElementById("posts-section").style.display = "none";
     document.getElementById("blog-section").style.display = "flex";
 }
 
 document.getElementById("blog-view").onclick = function() {
     document.getElementById("main-div").style.display = "none";
-    document.getElementById("sub-navbar").style.display = "flex";
+    document.getElementById("sub-navbar1").style.display = "flex";
     document.getElementById("posts-section").style.display = "none";
     document.getElementById("blog-section").style.display = "flex";
 }
 
 document.getElementById("sub-About").onclick = function(){
     document.getElementById("main-div").style.display = "flex";
-    document.getElementById("sub-navbar").style.display = "none";
+    document.getElementById("sub-navbar1").style.display = "none";
     document.getElementById("posts-section").style.display = "none";
     document.getElementById("blog-section").style.display = "none";
 }
